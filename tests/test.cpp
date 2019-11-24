568d56110a07cf01540f09200ab67b6a205e0f40
MoraPresence <od34156la@mail.ru>
2019-11-24 14:53:31 +0300

    version 1.1.0

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,34 +1,35 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
-#include "header.hpp"
+#include "main.h"
+TEST(Parse, Text){
+std::string json = "{\n"
+                   "    \"lastname\" : \"Ivanov\",\n"
+                   "    \"firstname\" : \"Ivan\",\n"
+                   "    \"age\" : 25,\n"
+                   "    \"islegal\" : false,\n"
+                   "    \"marks\" : [\n"
+                   "    \t4,5,5,5,2,3\n"
+                   "    ],\n"
+                   "    \"address\" : {\n"
+                   "    \t\"city\" : \"Moscow\",\n"
+                   "        \"street\" : \"Vozdvijenka\"\n"
+                   "    }\n"
+                   "}";
+Json object = Json::parse(json);
+EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
+EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
+EXPECT_EQ(std::any_cast<int>(object["age"]), 25);
 
-int main(){
-    std::string json = "{\n"
-                       "    \"lastname\" : \"Ivanov\",\n"
-                       "    \"firstname\" : \"Ivan\",\n"
-                       "    \"age\" : 25,\n"
-                       "    \"islegal\" : false,\n"
-                       "    \"marks\" : [\n"
-                       "    \t4,5,5,5,2,3\n"
-                       "    ],\n"
-                       "    \"address\" : {\n"
-                       "    \t\"city\" : \"Moscow\",\n"
-                       "        \"street\" : \"Vozdvijenka\"\n"
-                       "    }\n"
-                       "}";
-    Json object = Json::parse(json);
-    EXPECT_EQ(std::any_cast<std::string>(object["lastname"]), "Ivanov");
-    EXPECT_EQ(std::any_cast<bool>(object["islegal"]), false);
-    EXPECT_EQ(std::any_cast<int>(object["age"]), 25);
+auto marks = std::any_cast<Json>(object["marks"]);
+EXPECT_EQ(std::any_cast<int>(marks[0]), 4);
+EXPECT_EQ(std::any_cast<int>(marks[1]), 5);
 
-    auto marks = std::any_cast<Json>(object["marks"]);
-    EXPECT_EQ(std::any_cast<int>(marks[0]), 4);
-    EXPECT_EQ(std::any_cast<int>(marks[1]), 5);
-
-    auto address = std::any_cast<Json>(object["address"]);
-    EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
-    EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
-    return 0;
+auto address = std::any_cast<Json>(object["address"]);
+EXPECT_EQ(std::any_cast<std::string>(address["city"]), "Moscow");
+EXPECT_EQ(std::any_cast<std::string>(address["street"]), "Vozdvijenka");
 }
-
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
