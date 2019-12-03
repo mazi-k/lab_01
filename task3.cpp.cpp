4f113c50e1463c4d09cc7509185f52201402e0ac
MoraPresence <od34156la@mail.ru>
2019-12-03 11:01:11 +0300

    final

--- task3.cpp.cpp
+++ task3.cpp.cpp
@@ -9,7 +9,8 @@ int main() {
     auto j3 = json::parse("[\n"
                           "    [\"Si-9.15\", \"RTS-9.15\", \"GAZP-9.15\"],\n"
                           "    [100024, 100027, 100050],\n"
-                          "    [\"Futures contract for USD/RUB\", \"Futures contract for index RTS\", "
+                          "    [\"Futures contract for USD/RUB\", "
+                          "\"Futures contract for index RTS\", "
                           "\"Futures contract for Gazprom shares\"]\n"
                           "]");
     int tmp;
